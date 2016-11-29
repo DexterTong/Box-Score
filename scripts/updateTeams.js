85ab129beb4b744a8154b04aa166f22809c5d8a0
Dexter Tong <dyt217@nyu.edu>
2016-11-29 18:21:22 -0500

    update team info for nba package before updating database

--- scripts/updateTeams.js
+++ scripts/updateTeams.js
@@ -4,24 +4,30 @@ var nba = require('nba');
 var Team = require(path.join(__dirname, '..', 'models', 'team'));
 require(path.join(__dirname, '..', 'db'));
 
-//TODO: call updateTeams, use then or promise before updating team data
-//nba.updateTeams()
+nba.updateTeams().then(fulfill, reject);
 
-nba.teams.forEach(function(team){
-    var currentTeam = {
-        teamId: team.teamId,
-        abbreviation: team.abbreviation,
-        fullName: team.teamName,
-        name: team.simpleName,
-        city: team.location
-    };
-    var query = {teamId: currentTeam.teamId};
-    var update = currentTeam;
-    var options = {upsert: true, setDefaultsOnInsert: true};
-    Team.findOneAndUpdate(query, update, options, function(err, res){
-        if(err)
-            console.log(err);
+function fulfill() {
+    nba.teams.forEach(function(team){
+        var currentTeam = {
+            teamId: team.teamId,
+            abbreviation: team.abbreviation,
+            fullName: team.teamName,
+            name: team.simpleName,
+            city: team.location
+        };
+        var query = {teamId: currentTeam.teamId};
+        var update = currentTeam;
+        var options = {upsert: true, setDefaultsOnInsert: true};
+        Team.findOneAndUpdate(query, update, options, function(err){
+            if(err)
+                console.log(err);
+        });
     });
-});
+    console.log('Updated team data successfully');
+}
+
+function reject(){
+    console.log('Could not update team data, will not update database');
+}
 
 mongoose.disconnect();

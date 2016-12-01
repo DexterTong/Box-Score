d447430c641b6e3172302d9b06391adfa5db729d
Dexter Tong <dyt217@nyu.edu>
2016-12-01 18:04:57 -0500

    Consolidated team and player DB update script

--- scripts/updatePlayers.js
+++ scripts/updatePlayers.js
@@ -11,37 +11,65 @@ require(path.join(__dirname, '..', 'db'));
 
 var teamIdDict = {};
 
-nba.updatePlayers()
-    .then(
-        Team.find({}, function (err, teams) {
-            if (err)
-                console.log(err);
-            teams.forEach(function (team) {
-                teamIdDict[team.teamId] = team._id;
-            });
-        })
-    )
+nba.updateTeams()
     .then(function () {
         var promises = [];
-        nba.players.forEach(function (obj) {
-            var teamId = teamIdDict[obj.teamId];
-            var player = {
-                playerId: obj.playerId,
-                firstName: obj.firstName,
-                lastName: obj.lastName,
-                team: teamId
+        nba.teams.forEach(function (team) {
+            var currentTeam = {
+                teamId: team.teamId,
+                abbreviation: team.abbreviation,
+                name: team.simpleName,
+                city: team.location
             };
-            var query = {playerId: obj.playerId};
-            var update = player;
-            var options = {upsert: true, setDefaultsOnInsert: true, new: true};
-            promises.push(Player.findOneAndUpdate(query, update, options).exec()
-                .then(function (res) {
-                    Team.findByIdAndUpdate(teamId, {$addToSet: {"players": res._id}}).exec();
-                })
-            );
+            var query = {teamId: currentTeam.teamId};
+            var update = currentTeam;
+            var options = {upsert: true, setDefaultsOnInsert: true};
+            promises.push(Team.findOneAndUpdate(query, update, options).exec());
         });
         Promise.all(promises)
-            .then(function () {
-                mongoose.disconnect();
+            .then(function(){
+                console.log('Updated team data successfully');
+                updatePlayers();
             });
     });
+
+function updatePlayers() {
+    nba.updatePlayers()
+        .then(
+            Team.find({}, function (err, teams) {
+                if (err)
+                    console.log(err);
+                teams.forEach(function (team) {
+                    teamIdDict[team.teamId] = team._id;
+                });
+            })
+        )
+        .then(function () {
+            var promises = [];
+            nba.players.forEach(function (obj) {
+                var teamId = teamIdDict[obj.teamId];
+                var player = {
+                    playerId: obj.playerId,
+                    firstName: obj.firstName,
+                    lastName: obj.lastName,
+                    team: teamId
+                };
+                var query = {playerId: obj.playerId};
+                var update = player;
+                var options = {upsert: true, setDefaultsOnInsert: true, new: true};
+                promises.push(Player.findOneAndUpdate(query, update, options).exec()
+                    .then(function (res) {
+                        Team.findByIdAndUpdate(teamId, {$addToSet: {"players": res._id}}).exec();
+                    })
+                );
+            });
+            Promise.all(promises)
+                .then(function () {
+                    console.log('Updated player data successfully');
+                    mongoose.disconnect();
+                });
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}

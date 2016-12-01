cb1e41f720a250ce35fa46bc5551a3368b3a6d47
Dexter Tong <dyt217@nyu.edu>
2016-11-30 21:47:56 -0500

    finally wrangled the player update script into working

--- scripts/updatePlayers.js
+++ scripts/updatePlayers.js
@@ -8,19 +8,38 @@ require(path.join(__dirname, '..', 'db'));
 
 //TODO: call nba.updatePlayers beforehand, update after promise/then
 
-nba.players.forEach(function (obj) {
-    var player = {
-        playerId: obj.playerId,
-        firstName: obj.firstName,
-        lastName: obj.lastName
-    };
-    var query = {playerId: obj.playerId};
-    var update = player;
-    var options = {upsert: true, setDefaultsOnInsert: true};
-    Player.findOneAndUpdate(query, update, options, function (err) {
-        if (err)
-            console.log(err);
+
+var teamIdDict = {};
+
+Team.find({}, function (err, teams) {
+    if (err)
+        console.log(err);
+    teams.forEach(function (team) {
+        teamIdDict[team.teamId] = team._id;
     });
-});
-console.log('Updated player data successfully');
-mongoose.disconnect();
+}).then(updatePlayers).then(disconnect);
+
+function updatePlayers() {
+    console.log(teamIdDict);
+    nba.players.forEach(function (obj) {
+        var player = {
+            playerId: obj.playerId,
+            firstName: obj.firstName,
+            lastName: obj.lastName,
+            team: teamIdDict[obj.teamId]
+        };
+        //console.log(teamIdDict[obj.teamId]);
+        var query = {playerId: obj.playerId};
+        var update = player;
+        var options = {upsert: true, setDefaultsOnInsert: true};
+        Player.findOneAndUpdate(query, update, options, function (err) {
+            if (err)
+                console.log(err);
+        });
+    });
+    console.log('Updated player data successfully');
+}
+
+function disconnect() {
+    mongoose.disconnect();
+}

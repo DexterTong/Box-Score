71b15f72abb1709cd6e4b2366dad1ab4f0d59955
Dexter Tong <dyt217@nyu.edu>
2016-12-01 22:19:08 -0500

    to make linking to things easier

--- helpers/handlebarHelpers.js
+++ helpers/handlebarHelpers.js
@@ -7,4 +7,24 @@ hbs.registerHelper('ifequal', function(arg1, arg2){
         return 'selected';
 });
 
+hbs.registerHelper('linkUser', function(username){
+   return new hbs.SafeString('<a href="/user/' + username + '">' + username + '</a>');
+});
+
+hbs.registerHelper('linkGame', function(gameId){
+    return new hbs.SafeString('<a href="/game/' + gameId + '">' + gameId + '</a>');
+});
+
+hbs.registerHelper('linkTeam', function(teamName){
+    return new hbs.SafeString('<a href="/team/' + teamName + '">' + teamName + '</a>');
+});
+
+hbs.registerHelper('linkPlayer', function(playerId){
+    return new hbs.SafeString('<a href="/player/' + playerId + '">' + playerId + '</a>');
+});
+
+hbs.registerHelper('linkPrediction', function(predictionId){
+    return new hbs.SafeString('<a href="/prediction/' + predictionId + '">' + predictionId + '</a>');
+});
+
 module.exports = hbs;

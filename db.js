c0201a7edfa66aa45d4e20f025aaad8bacacb7ad
Dexter Tong <dyt217@nyu.edu>
2016-12-07 12:50:22 -0500

    forgot to change a variable name

--- db.js
+++ db.js
@@ -1,17 +1,27 @@
 var mongoose = require('mongoose');
 
 var connectionString;
+var fs;
+var path;
+var prodEnv;
 
-if (process.env.NODE_ENV == 'PRODUCTION') {
-
-    var fs = require('fs');
-    var path = require('path');
-    var prodEnv = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'))).prodEnv;
+if (process.env.NODE_ENV == 'PRODUCTION_A') {
+    fs = require('fs');
+    path = require('path');
+    prodEnv = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'))).prod_a;
     connectionString = 'mongodb://' + prodEnv.username + ':' + prodEnv.password + '@'
         + prodEnv.address + '/' + prodEnv.database;
 }
 else
-    connectionString = 'mongodb://localhost/box-score';
+    if (process.env.NODE_ENV == 'PRODUCTION_B') {
+        fs = require('fs');
+        path = require('path');
+        prodEnv = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'))).prod_b;
+        connectionString = 'mongodb://' + prodEnv.username + ':' + prodEnv.password + '@'
+            + prodEnv.address + '/' + prodEnv.database;
+    }
+    else
+        connectionString = 'mongodb://localhost/box-score';
 
 mongoose.connect(connectionString, function (err) {
     if (err)

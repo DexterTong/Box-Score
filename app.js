fbb2e4078a9b74575f6173ac800d698dde63d7a8
Dexter Tong <dyt217@nyu.edu>
2016-12-12 21:03:34 -0500

    production instances no longer leak stack trace

--- app.js
+++ app.js
@@ -55,21 +55,7 @@ app.use(function (req, res, next) {
     next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: err
-    });
-});
-
-// production error handler
-// no stacktraces leaked to user
-if (process.env.NODE_ENV === 'PRODUCTION') {
+if (process.env.NODE_ENV === 'PRODUCTION_A' || process.env.NODE_ENV === 'PRODUCTION_B') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
@@ -78,5 +64,14 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
         });
     });
 }
+else
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+
 
 module.exports = app;
